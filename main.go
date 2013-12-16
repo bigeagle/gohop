082179d91fdf6cd103422011c9bb448cd21af9f4
bigeagle <justin.w.xd@gmail.com>
2013-12-16 11:39:32 +0800

    fix example config file bugs

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ import (
 var srvMode, cltMode, debug, getVersion bool
 var cfgFile string
 
-var VERSION = "0.3alpha2"
+var VERSION = "0.3.1-dev"
 
 func main() {
     flag.BoolVar(&getVersion, "version", false, "Get Version info")
